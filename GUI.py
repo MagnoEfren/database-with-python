c81416f624166243c7da2f44c4e27074273da3f5
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2023-04-06 22:10:39 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-
 # Form implementation generated from reading ui file 'GUI.ui'
-#
 # Created by: PyQt5 UI code generator 5.9.2
-#
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
